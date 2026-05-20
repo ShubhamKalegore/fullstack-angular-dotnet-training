cbbd1baa731c871520081689bd6e186a3f4a1fb0
Shubham Kalegore <shubhamkalegore88@gmail.com>
2026-05-20 11:07:57 +0530

    removed old project and added new one

--- angular/src/app/app.config.ts
+++ angular/src/app/app.config.ts
@@ -1,15 +1,8 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
